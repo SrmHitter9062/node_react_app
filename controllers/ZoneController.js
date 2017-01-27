9d0cab896a80ffe158fa66be87fd9cdc5fca09c9
Siyaram Malav <malavsiya@gmail.com>
2017-01-28 00:39:07 +0530

    Mongoose connection (#1)
    * basic api integration
    
    * small

--- controllers/ZoneController.js
+++ controllers/ZoneController.js
@@ -4,23 +4,60 @@ module.exports = {
   find:function(params,callback){
     Zone.find(params,function(err,zones){
       if(err){
-        return callback(err,null)
+        callback(err,null)
+        return
       }
       callback(null,zones)
     })
 
   },
-  findById:function(){
+  findById:function(id,callback){
+    Zone.findById(id,function(err,zones){
+      if(err){
+        callback(err,null)
+        return
+      }
+      callback(null,zones)
+    })
 
   },
-  create:function(){
+  create:function(params,callback){
+    var zipcodes = params['zipCodes'].split(',');
+    var zipArray = [];
+    zipcodes.forEach(function(zipCode){
+      zipArray.push(zipCode.trim());
+      //validate zipcodes(should be 5 digits)
+    })
+    params['zipCodes'] = zipcodes;
+    Zone.create(params,function(err,zone){
+      if(err){
+        callback(err,null)
+        return
+      }
+      console.log('zone created!!');
+      callback(null,zone)
+    })
 
   },
-  update:function(){
+  update:function(id,params,callback){
+    Zone.findByIdAndUpdate(id,params,{new:true},function(err,zone){
+      if(err){
+        callback(err,null);
+        return;
+      }
+      console.log('zone updated!!');
+      callback(null,zone)
+    })
 
   },
-  destroy:function(){
-
+  delete:function(id,callback){
+    Zone.findByIdAndRemove(id,function(err){
+      if(err){
+        callback(err,null);
+        return;
+      }
+      callback(null,null);
+    })
   }
 
 }
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var router = express.Router()
+var ZoneController = require("../controllers/ZoneController")
+var controllers = require('../controllers')
 
 router.get('/getMovies',function(req,res,next){
-
+  // console.log("your query is ",req)
   res.json({
     confirmation : 'success',
     data : {'movies':[]}
@@ -11,10 +13,89 @@ router.get('/getMovies',function(req,res,next){
 
 router.get('/:resource',function(req,res,next){
   var resource = req.params.resource
-  res.json({
-    confirmation : 'success',
-    resource:resource
+  var controller = controllers[resource];
+  console.log('controller resource is ->',resource);
+ /* if controller does not exists*/
+  if(controller == null){
+    res.json({
+      status:'fail',
+      message: 'Invalid Resource Request : '+ resource
+    })
+    return
+  }
+  controller.find(req.query,function(err,results){
+    if(err){
+      res.json({
+        status:'fail',
+        message : 'Not Found'
+      })
+      return
+    }
+    res.json({
+      status:'success',
+      result:results
+    })
+
   })
+
+})
+
+router.get('/:resource/:id',function(req,res,next){
+  var resource = req.params.resource
+  var id = req.params.id
+  var controller = controllers[resource];
+  /* if controller does not exists*/
+   if(controller == null){
+     res.json({
+       status:'fail',
+       message: 'Invalid Resource Request : '+ resource
+     })
+     return
+   }
+  controller.findById(id,function(err,result){
+    if(err){
+      res.json({
+        status:'fail',
+        message : 'Not Found'
+      })
+      return
+    }
+    res.json({
+      status:'success',
+      result:result
+    })
+  })
+
+})
+
+router.post('/:resource',function(req,res,next){
+  var resource = req.params.resource
+  console.log('data is ',req.body);
+  // req.body.zipCodes = req.body.zipCodes.split(',');
+  var controller = controllers[resource];
+  /* if controller does not exists*/
+   if(controller == null){
+     res.json({
+       status:'fail',
+       message: 'Invalid Resource Request : '+ resource
+     })
+     return
+   }
+
+  controller.create(req.body,function(err,result){
+    if(err){
+      res.json({
+        status:'fail',
+        message:err
+      })
+      return
+    }
+    res.json({
+      status:'success',
+      message:result
+    })
+  });
+
 })
 
 
